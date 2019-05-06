d5eeb952206d942dd2bc6e11327d7b0bb8317617
Mario <codingphs@gmail.com>
2019-05-06 16:58:29 -0500

    Add filetype, update code
    Revision 2

--- CLIENT.sql
+++ CLIENT.sql
@@ -9,37 +9,35 @@
  --*************************************************           
  --THIS CODE CREATES THE CLIENT TABLESPACE                      
  --*************************************************           
- CREATE TABLESPACE P001TSAB IN P070DB USING STOGROUP UNTDISK;  
+ CREATE TABLESPACE P001TSCL IN P070DB USING STOGROUP UNTDISK;
 
 
 --*************************************************          
 --THIS CODE CREATES THE CLIENT TABLE                          
 --*************************************************          
- CREATE TABLE CLIENT                                         
- (CL_ID INTEGER NOT NULL,                                    
-  CL_NAME CHAR(25) NOT NULL,                                 
-  CL_CONTACT CHAR (25) NOT NULL,                             
-  CL_PHONE CHAR(25) NOT NULL,                                
-  PRIMARY KEY (CL_ID))                                       
-  IN P070DB.P001TSAB;                                        
+ CREATE TABLE CLIENT         
+(CL_ID INTEGER NOT NULL,    
+ CL_NAME CHAR(25) NOT NULL, 
+ CL_CNTCT CHAR(25) NOT NULL,
+ CL_PHONE CHAR(25) NOT NULL,
+ PRIMARY KEY (CL_ID))       
+ IN P070DB.P001TSCL;                                             
 
 --*************************************************
 --THIS CODE CREATES THE PK INDEX ON CLIENT
 --*************************************************
---CREATE PK INDEX ON CLIENT 
-CREATE UNIQUE INDEX UNTP001.X1AB
-ON UNTP070.CLIENT (CL_ID)    
-USING STOGROUP UNTDISK;         
-COMMIT;                                                   
-
+ CREATE UNIQUE INDEX UNTP001.X1CL
+ ON UNTP070.CLIENT(CL_ID)        
+ USING STOGROUP UNTDISK;                                             
+ COMMIT;
 
 --*************************************************
 --THIS CODE WILL POPULATE THE CLIENT TABLE
 --*************************************************                            
 --INSERT INTO CONSULTANT VALUES(CID, NAME, CONTACT, PHONE);
 
-INSERT INTO CLIENT VALUES(1, 'MARVEL STUDIOS', 'STAN LEE', '999000111');
-INSERT INTO CLIENT VALUES(2, 'CAPCOM', 'PAC MAN', '4560401342');
-INSERT INTO CLIENT VALUES(3, 'DISNEY', 'DARTH VADER', '1112334563');
-INSERT INTO CLIENT VALUES(4, 'UNIVERSAL', 'BRUCE BANNER', '1213141516');
-INSERT INTO CLIENT VALUES(5, 'SONY', 'PETER PARKER', '123123456'); 
+ INSERT INTO CLIENT VALUES(1, 'MARVEL STUDIOS', 'STAN LEE', '99900011');
+ INSERT INTO CLIENT VALUES(2, 'CAPCOM', 'MANNY PAC', '4567890123');     
+ INSERT INTO CLIENT VALUES(3, 'DISNEY', 'MARTH VADER', '6665551234');   
+ INSERT INTO CLIENT VALUES(4, 'UNIVERSAL', 'BRUCE BANNER', '159753014');
+ INSERT INTO CLIENT VALUES(5, 'SONY', 'PETER PARKER', '7412369845');    
